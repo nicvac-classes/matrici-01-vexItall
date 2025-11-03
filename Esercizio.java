a0728c4caf22bb54043a0fd668e88a6ac3eec43d
Benny <fraddosio.patti67@gmail.com>
2025-11-03 15:34:48 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -2,15 +2,42 @@
 public class Esercizio {
     
     public static void esercizio1() {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO 1
+        int[][] M = new int[3][5];
+
+        M[1][0] = 7;
+        M[1][1] = 7;
+        M[1][2] = 7;
+        M[1][3] = 7;
+        M[1][4] = 7;
+
+        UtilsMatrice.visualizza(M);
     }
     
     public static void esercizio2() {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO 2
+        int[][] M = new int[3][5];
+
+        int j = 0;
+
+        while(j < 5){
+            M[1][j] = 7;
+            ++j;
+        }
+
+        UtilsMatrice.visualizza(M);
     }
     
     public static void esercizio3() {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO 3
+
+        int[][] M = new int[3][5];
+
+        int i = 0;
+        
+        while(i < 3){
+            M[i][3] = 7;
+            ++i;
+        }
+
+        UtilsMatrice.visualizza(M);
     }
     
     public static void main(String[] args) {
